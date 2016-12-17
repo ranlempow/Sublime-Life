8ba527faced9390c1429601f4fac531bc733cd43
ranlempow <ranlempow@users.noreply.github.com>
2016-12-17 21:00:23 +0800

    eliminate the dependent on global variable

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
 import sublime, sublime_plugin
-import string
-
-
-class PleasurazyAPICompletionsPackage():
-    def init(self):
-        self.api = {}
-        self.settings = sublime.load_settings('sbc-setting.sublime-settings')
-        self.API_Setup = self.settings.get('completion_active_list')
-
-        # Caching completions
-        if self.API_Setup:
-            for API_Keyword in self.API_Setup:
-                self.api[API_Keyword] = sublime.load_settings('sbc-api-' + API_Keyword + '.sublime-settings')
-
-
-# In Sublime Text 3 things are loaded async, using plugin_loaded() callback before try accessing.
-pleasurazy = PleasurazyAPICompletionsPackage()
-
-def plugin_loaded():
-    global pleasurazy
-    pleasurazy.init()
-
 
 class CompletionsPackageEventListener(sublime_plugin.ViewEventListener):
     def __init__(self, view):
         super().__init__(view)
-
+        self.settings = sublime.load_settings('sbc-setting.sublime-settings')
 
     def on_query_completions(self, prefix, locations):
+
         self.completions = []
-        for API_Keyword in pleasurazy.api:
+        for API_Keyword in self.settings.get('completion_active_list', []):
             # If completion active
-            if (pleasurazy.API_Setup and pleasurazy.API_Setup.get(API_Keyword)) or (pleasurazy.settings.get('completion_active_extend_list') and pleasurazy.settings.get('completion_active_extend_list').get(API_Keyword)):
-                scope = pleasurazy.api[API_Keyword].get('scope')
+            if self.settings.get('completion_active_list').get(API_Keyword):
+                sbc_api = sublime.load_settings('sbc-api-' + API_Keyword + '.sublime-settings')
+
+                scope = sbc_api.get('scope')
                 if scope and self.view.match_selector(locations[0], scope):
-                    self.completions += pleasurazy.api[API_Keyword].get('completions')
+                    self.completions += sbc_api.get('completions')
 
         if not self.completions:
             return []
@@ -43,7 +24,7 @@ class CompletionsPackageEventListener(sublime_plugin.ViewEventListener):
         compDefault = [self.view.extract_completions(prefix)]
         compDefault = [(item, item) for sublist in compDefault for item in sublist if len(item) > 3]
         compDefault = list(set(compDefault))
-        completions = list(self.completions)
+
         completions = [tuple(attr) for attr in self.completions]
         completions.extend(compDefault)
-        return (completions)
+        return completions
