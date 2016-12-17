d5e2aad6aff8e5fffb6762447f961578ab96faa2
ranlempow <ranlempow@users.noreply.github.com>
2016-12-17 20:48:26 +0800

    eventlistener now is per-view

--- main.py
+++ main.py
@@ -22,25 +22,25 @@ def plugin_loaded():
     pleasurazy.init()
 
 
+class CompletionsPackageEventListener(sublime_plugin.ViewEventListener):
+    def __init__(self, view):
+        super().__init__(view)
 
-class PleasurazyAPICompletionsPackageEventListener(sublime_plugin.EventListener):
-    global pleasurazy
 
-    def on_query_completions(self, view, prefix, locations):
+    def on_query_completions(self, prefix, locations):
         self.completions = []
-
         for API_Keyword in pleasurazy.api:
             # If completion active
             if (pleasurazy.API_Setup and pleasurazy.API_Setup.get(API_Keyword)) or (pleasurazy.settings.get('completion_active_extend_list') and pleasurazy.settings.get('completion_active_extend_list').get(API_Keyword)):
                 scope = pleasurazy.api[API_Keyword].get('scope')
-                if scope and view.match_selector(locations[0], scope):
+                if scope and self.view.match_selector(locations[0], scope):
                     self.completions += pleasurazy.api[API_Keyword].get('completions')
 
         if not self.completions:
             return []
 
         # extend word-completions to auto-completions
-        compDefault = [view.extract_completions(prefix)]
+        compDefault = [self.view.extract_completions(prefix)]
         compDefault = [(item, item) for sublist in compDefault for item in sublist if len(item) > 3]
         compDefault = list(set(compDefault))
         completions = list(self.completions)
